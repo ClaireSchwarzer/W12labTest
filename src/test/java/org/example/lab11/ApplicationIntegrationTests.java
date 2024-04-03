db9b039dcef7acdbd40df9abf0f00f264895b68d
laiqixiao <laiqixiao214@gmai.com>
2024-04-03 18:48:15 -0400

    Trying the integration test

--- src/test/java/org/example/lab11/ApplicationIntegrationTests.java
+++ src/test/java/org/example/lab11/ApplicationIntegrationTests.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ApplicationIntegrationTests {
+public class ApplicationIntegrationTests {//NEW integration test
 
     @Autowired
     private TestRestTemplate restTemplate;
